742322b0e831e1b42427f0b13e1d08bfb9bd110a
arcanar7 <arcanar7@gmail.com>
2020-04-01 14:16:43 +0400

    feat: implement task solution

---
+++ src/components/buttonAlternative.js
@@ -0,0 +1,27 @@
+import Button from './button.js'
+
+class ButtonAlternative extends Button {
+  constructor(
+    text = { en: 'a', ru: 'а' },
+    wide = 'normal',
+    lang = 'en',
+    altText = { en: 'A', ru: 'А' },
+    code = 'KeyA',
+  ) {
+    super(text, wide, lang, altText, code)
+    this.shifted = false
+  }
+
+  shift() {
+    // eslint-disable-next-line no-extra-semi
+    ;[this.text, this.altText] = [this.altText, this.text]
+    this.changeText()
+    this.node.classList.toggle('shift')
+  }
+
+  caps() {
+    this.node.classList.toggle('shift')
+  }
+}
+
+export default ButtonAlternative
